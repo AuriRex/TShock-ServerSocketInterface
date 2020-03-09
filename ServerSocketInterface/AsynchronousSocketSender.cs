3668ba9f0b81ec51adfdac3f3beab61824abade4
AuriRex <xaurirexx@gmail.com>
2020-03-09 13:18:18 +0100

    init

---
+++ ServerSocketInterface/AsynchronousSocketSender.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerSocketInterface {
+    public class AsynchronousSocketSender {
+
+        private static Queue<String> queue = new Queue<String>();
+
+        private static String msg = "";
+
+        private static Boolean running = true;
+
+        public static void StartLoop() {
+            while(running) {
+
+                while(queue.Count < 1) {
+                    Thread.Sleep(100);
+                    if (!running) return;
+                }
+
+                msg = queue.Dequeue();
+
+                SendToExternalServer(msg);
+
+            }
+        }
+
+        public static void Send(String message) {
+            queue.Enqueue(message);
+        }
+
+        public static void Stop() {
+            running = false;
+        }
+
+        private static void SendToExternalServer(String message) {
+            try {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                Int32 port = 11001;
+                TcpClient client = new TcpClient("localhost", port);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.Unicode.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            } catch (ArgumentNullException e) {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            } catch (SocketException e) {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+        }
+    }
+}
---
+++ ServerSocketInterface/ServerSocketInterface.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI.Hooks;
+
+namespace ServerSocketInterface {
+    /// <summary>
+    /// The main plugin class should always be decorated with an ApiVersion attribute. The current API Version is 1.25
+    /// </summary>
+    [ApiVersion(2, 1)]
+    public class ServerSocketInterface : TerrariaPlugin
+    {
+
+        public static ServerSocketInterface instance;
+
+        /// <summary>
+        /// The name of the plugin.
+        /// </summary>
+        public override string Name => "Server Socket Interface";
+
+        /// <summary>
+        /// The version of the plugin in its current state.
+        /// </summary>
+        public override Version Version => new Version(1, 2, 0);
+
+        /// <summary>
+        /// The author(s) of the plugin.
+        /// </summary>
+        public override string Author => "AuriRex";
+
+        /// <summary>
+        /// A short, one-line, description of the plugin's purpose.
+        /// </summary>
+        public override string Description => "Creates a Socket to receive and send data to other local programms.";
+
+        /// <summary>
+        /// The plugin's constructor
+        /// Set your plugin's order (optional) and any other constructor logic here
+        /// </summary>
+        public ServerSocketInterface(Main game) : base(game)
+        {
+            Order = 100;
+        }
+
+        Thread socketThread = null;
+        Thread senderThread = null;
+
+        /// <summary>
+        /// Performs plugin initialization logic.
+        /// Add your hooks, config file read/writes, etc here
+        /// </summary>
+        public override void Initialize() //First method TShock runs when the plugin is loaded
+        {
+            instance = this;
+            ServerApi.Hooks.ServerJoin.Register(this, OnServerJoin);
+            ServerApi.Hooks.ServerLeave.Register(this, OnServerLeave);
+            ServerApi.Hooks.ServerChat.Register(this, OnServerChat);
+            ServerApi.Hooks.GamePostInitialize.Register(this, OnGamePostInitialize);
+            ServerApi.Hooks.ServerBroadcast.Register(this, OnServerBroadcast);
+
+            //TShockAPI.Utils.Instance.
+            socketThread = new Thread(AsynchronousSocketListener.StartListening);
+            socketThread.Start();
+            senderThread = new Thread(AsynchronousSocketSender.StartLoop);
+            senderThread.Start();
+            AsynchronousSocketSender.Send("ServerInit: true");
+            // Console.WriteLine("I'm not stuck :3");
+        }
+
+        private void OnServerBroadcast(ServerBroadcastEventArgs args) {
+            if(args != null) {
+                if (args.Message == null) return;
+                if (args.Color == null) return;
+                AsynchronousSocketSender.Send($"ServerBroadcast: {args.Message}\n{args.Color.ToString()}");
+            }
+            
+        }
+
+        private void OnGamePostInitialize(EventArgs args) {
+            AsynchronousSocketSender.Send("GamePostInitialize: true");
+        }
+
+        public void SendMsg(String msg) {
+            if (TShockAPI.TShock.Players != null)
+                foreach (TShockAPI.TSPlayer ply in TShockAPI.TShock.Players) {
+                    if(ply != null)
+                        ply.SendSuccessMessage(msg);
+                }
+        }
+
+        public String GetOnlinePlayers() {
+            String ret = "";
+            if(TShockAPI.TShock.Players != null)
+                foreach (TShockAPI.TSPlayer ply in TShockAPI.TShock.Players) {
+                    if(ply != null)
+                        ret += ply.Name + "\n" + ply.IP + "\n";
+                }
+            if (ret.Equals(""))
+                ret = "None";
+            return ret;
+        }
+
+        void OnServerJoin(JoinEventArgs args) {
+            if (args != null) {
+                if (TShockAPI.TShock.Players[args.Who] != null) {
+                    Console.WriteLine($"ServerJoin: {TShockAPI.TShock.Players[args.Who].Name}");
+                    AsynchronousSocketSender.Send($"ServerJoin: {TShockAPI.TShock.Players[args.Who].Name}\n{TShockAPI.TShock.Players[args.Who].IP}");
+                }
+            }
+        }
+        
+        void OnServerLeave(LeaveEventArgs args) {
+            if(args != null) {
+                if(TShockAPI.TShock.Players[args.Who] != null) {
+                    AsynchronousSocketSender.Send($"ServerLeave: {TShockAPI.TShock.Players[args.Who].Name}\n{TShockAPI.TShock.Players[args.Who].IP}");
+                }
+            }
+        }
+
+        void OnServerChat(ServerChatEventArgs args) {
+            if (args != null) {
+                if (TShockAPI.TShock.Players[args.Who] != null) {
+                    AsynchronousSocketSender.Send($"ServerChat: {TShockAPI.TShock.Players[args.Who].Name}\n{args.Text}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Performs plugin cleanup logic
+        /// Remove your hooks and perform general cleanup here
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                AsynchronousSocketSender.Send("ServerShutdown: true");
+                //unhook
+                //dispose child objects
+                //set large objects to null
+                ServerApi.Hooks.ServerJoin.Deregister(this, OnServerJoin);
+                ServerApi.Hooks.ServerLeave.Deregister(this, OnServerLeave);
+                ServerApi.Hooks.ServerChat.Deregister(this, OnServerChat);
+                ServerApi.Hooks.GamePostInitialize.Deregister(this, OnGamePostInitialize);
+                ServerApi.Hooks.ServerBroadcast.Deregister(this, OnServerBroadcast);
+
+
+                try {
+                    if (socketThread != null)
+                        socketThread.Abort();
+                } catch(Exception ex) {
+
+                }
+                AsynchronousSocketSender.Stop();
+
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
